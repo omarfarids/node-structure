232a1566770ed72030d4fc07fac9013bfc95d13c
omarfarids <omar.farid007@gmail.com>
2023-12-12 10:05:49 +0200

    remove ignored files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // ----------- exports --------------
 var createError = require("http-errors");
-const mongoose = require("mongoose");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 // ------------- internal exports ------------
 var indexRouter = require("./routes/index");
@@ -18,6 +18,17 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// ----------- database -----------
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
 // ----------- routes -----------
 app.use("/", indexRouter);
 
@@ -28,17 +39,6 @@ app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-
-// ------------- db --------------
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"))
-  .catch((err) => {
-    console.log(err);
-  });
 
 // ------- catch 404 and forward to error handler ------
 app.use(function (req, res, next) {
